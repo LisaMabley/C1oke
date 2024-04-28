0036e12fa24b9ac25bef0c7d7c5ed9aa9cc2e8c2
Lisa Mabley <lisamabley@gmail.com>
2024-04-27 19:14:51 -0500

    feat: complete encode functionality, stub out form

--- c1oke-app/src/App.js
+++ c1oke-app/src/App.js
@@ -9,39 +9,42 @@ function App() {
 
   function encodeMessage(key, message) {
     const keyArray = key.split(' ');
-    const keyLength = keyArray.length;
     const spaceIndex = keyArray.shift();
     const encodeArray = getEncodeArray(spaceIndex);
     const messageArray = message.split('');
     let keyIndex = 0;
     let result = '';
-    console.log(keyArray);
     messageArray.forEach((letter, index) => {
-      if (keyIndex > keyLength) {
+      if (keyIndex + 1 === keyArray.length) {
         keyIndex = 0;
-      } else {
-        keyIndex += 1;
       }
       const shiftIndex = parseInt(keyArray[keyIndex]);
       const letterIndex = encodeArray.indexOf(letter);
-      const totalShift = letterIndex + shiftIndex;
+      let totalShift = letterIndex + shiftIndex;
+      while (totalShift < 0) {
+        totalShift += encodeArray.length;
+      }
+      while (totalShift + 1 > encodeArray.length) {
+        totalShift -= encodeArray.length;
+      }
       const encodedLetter = encodeArray[totalShift];
-      console.log('shift index', shiftIndex);
-      console.log('total shift', totalShift);
       console.log(`${letter} = ${encodedLetter}`);
-      result = result.concat(encodedLetter)
+      result = result.concat(encodedLetter);
+      keyIndex += 1;
     });
     return result;
   }
 
   const key = '7 9 12 0 -4 -4 3 -2';
-  const message = 'hello world';
+  const message = 'great news family we are having takeout tonight';
 
   const encodedMessage = encodeMessage(key, message);
   
   return (
     <div className="App">
       <header className="App-header">
+        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
         {encodedMessage}
       </header>
     </div>
