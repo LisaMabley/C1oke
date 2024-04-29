2d529d5e36ed2f55d352f6914b725bc39f25369d
Lisa Mabley <lisamabley@gmail.com>
2024-04-28 19:19:43 -0500

    feat: add decode functionality

--- c1oke-app/src/App.js
+++ c1oke-app/src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import './App.css';
 import { Box, Button, Card, Tabs, Tab, TextField, Typography } from '@mui/material';
 
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -23,9 +25,11 @@ function TabPanel(props) {
 }
 
 function App() {
-  const [ encodedMessage, setEncodedMessage ] = useState('');
   const [ key, setKey ] = useState('');
-  const [ inputMessage, setInputMessage ] = useState('');
+  const [ messageToEncode, setMessageToEncode ] = useState('');
+  const [ encodedMessage, setEncodedMessage ] = useState('');
+  const [ messageToDecode, setMessageToDecode ] = useState('');
+  const [ decodedMessage, setDecodedMessage ] = useState('');
   const [selectedTab, setSelectedTab] = useState(0);
 
   const handleChange = (event, newValue) => {
@@ -33,12 +37,44 @@ function App() {
   };
 
   function getEncodeArray(spaceIndex) {
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     alphabet.splice(spaceIndex - 1, 0, ' ');
     return alphabet;
   }
 
   function encodeMessage(key, message) {
+    const keyArray = key.split(' ');
+    const spaceIndex = keyArray.shift();
+    const encodeArray = getEncodeArray(spaceIndex);
+    const messageArray = message.split('');
+    let keyIndex = 0;
+    let result = '';
+    messageArray.forEach((letter, index) => {
+      if (keyIndex + 1 === keyArray.length) {
+        keyIndex = 0;
+      }
+      const shiftIndex = parseInt(keyArray[keyIndex]);
+      const letterIndex = encodeArray.indexOf(letter);
+      let totalShift = letterIndex - shiftIndex;
+      while (totalShift < 0) {
+        totalShift += encodeArray.length;
+      }
+      while (totalShift + 1 > encodeArray.length) {
+        totalShift -= encodeArray.length;
+      }
+      const encodedLetter = encodeArray[totalShift];
+      console.log(`${letter} = ${encodedLetter}`);
+      result = result.concat(encodedLetter);
+      keyIndex += 1;
+    });
+    return result;
+  }
+
+  function onEncodeSubmit() {
+    const result = encodeMessage(key, messageToEncode);
+    setEncodedMessage(result);
+  }
+
+  function decodeMessage(key, message) {
     const keyArray = key.split(' ');
     const spaceIndex = keyArray.shift();
     const encodeArray = getEncodeArray(spaceIndex);
@@ -66,9 +102,9 @@ function App() {
     return result;
   }
 
-  function onSubmit() {
-    const result = encodeMessage(key, inputMessage);
-    setEncodedMessage(result);
+  function onDecodeSubmit() {
+    const result = decodeMessage(key, messageToDecode);
+    setDecodedMessage(result);
   }
 
   return (
@@ -97,25 +133,53 @@ function App() {
             <TextField 
               sx={{ margin: '16px', width: "60%" }} 
               id="message" 
-              value={inputMessage} 
+              value={messageToEncode} 
               label="Message" 
               variant="outlined" 
-              placeholder='Message'
-              onChange={(e) => setInputMessage(e.target.value.toLowerCase())}
+              placeholder='Message to encode'
+              onChange={(e) => setMessageToEncode(e.target.value.toLowerCase())}
               helperText="No punctuation or numbers allowed (yet)" 
             />
-            <Button sx={{ margin: '16px', width: "60%" }} variant="contained" onClick={onSubmit}>Submit</Button>
+            <Button sx={{ margin: '16px', width: "60%" }} variant="contained" onClick={onEncodeSubmit}>Submit</Button>
         </form>
-          <Box>
-            <Card variant="outlined" sx={{ borderRadius: '6px', my: 3, mx: 'auto', padding: 2, width: '58%', backgroundColor: '#555555' }}>
-              <Typography sx={{ color: '#414141' }} variant="h4">
+          {  encodedMessage && <Box>
+            <Card variant="outlined" sx={{ borderRadius: '6px', my: 3, mx: 'auto', padding: 2, width: '58%', backgroundColor: '#999999' }}>
+              <Typography sx={{ color: '#FFFFFF' }} variant="h4">
                 {encodedMessage}
               </Typography>
             </Card>
-          </Box>
+          </Box>}
       </TabPanel>
       <TabPanel value={selectedTab} index={1}>
         <Typography variant="h2">Decode a Message</Typography>
+        <form sx={{ width: '60%' }}>
+          <TextField 
+              sx={{ margin: '16px', width: "60%" }} 
+              id="key" value={key} 
+              label="Key" 
+              variant="outlined" 
+              placeholder='Key' 
+              onChange={(e) => setKey(e.target.value)}
+              helperText="The key your message was encoded with"
+            />
+            <TextField 
+              sx={{ margin: '16px', width: "60%" }} 
+              id="message" 
+              value={messageToDecode} 
+              label="Message to decode" 
+              variant="outlined" 
+              placeholder='Message'
+              onChange={(e) => setMessageToDecode(e.target.value.toLowerCase())}
+            />
+            <Button sx={{ margin: '16px', width: "60%" }} variant="contained" onClick={onDecodeSubmit}>Submit</Button>
+        </form>
+          {  decodedMessage && <Box>
+            <Card variant="outlined" sx={{ borderRadius: '6px', my: 3, mx: 'auto', padding: 2, width: '58%', backgroundColor: '#999999' }}>
+              <Typography sx={{ color: '#FFFFFF' }} variant="h4">
+                {decodedMessage}
+              </Typography>
+            </Card>
+          </Box>}
       </TabPanel>
     </div>
   );
